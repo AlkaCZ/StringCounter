98bac50d852a6484068a604bf98cc6deff34c744
Meterologická stanice <petrjelinek35@seznam.cz>
2020-01-06 15:49:39 +0100

    Přidejte soubory projektu.

---
+++ StringCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
